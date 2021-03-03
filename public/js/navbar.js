60372499effc1323926ec558df16cfae1f0051e8
Manuel <mosquera.manuel2011@gmail.com>
2021-03-02 22:13:16 -0500

    Burger menu added in the responsive mobile view

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -17,3 +17,31 @@ navLinks.forEach((nav, i) => {
         activePage(i)
     })
 })
+
+
+
+// Burger menu
+
+const navContent = document.querySelector('#navbar-content')
+const navToggle = document.querySelector('#navbar-toggle')
+
+let navActive = false
+
+navToggle.addEventListener('click', (event) => {
+    navActive = !navActive
+
+    if (navActive) {
+        navContent.classList.add('open')
+    } else {
+        navContent.classList.remove('open')
+    }
+
+    event.stopPropagation()
+})
+
+document.body.addEventListener('click', () => {
+    if (navActive) {
+        navContent.classList.remove('open')
+        navActive = false
+    }
+})
